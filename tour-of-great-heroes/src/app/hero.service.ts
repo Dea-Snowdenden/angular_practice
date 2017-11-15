eb15b745c7c24f42b74fc731a083224ebbd956ec
deazee101 <deazee101@gmail.com>
2017-11-15 14:50:28 +0800

    add

--- tour-of-great-heroes/src/app/hero.service.ts
+++ tour-of-great-heroes/src/app/hero.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 import { Observable } from 'rxjs/Rx';
 import { of } from 'rxjs/observable/of';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
+
+    /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+  this.messageService.add('HeroService: ' + message);
+  private heroesUrl = 'api/heroes';  // URL to web api
+}
 
   getHeroes(): Observable<Hero[]> {
     // Todo: send the message _after_ fetching the heroes
