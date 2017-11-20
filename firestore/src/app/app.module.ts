38517feacd01b81fc3981c1da3fb33cbf6c06d8c
deazee101 <deazee101@gmail.com>
2017-11-20 17:50:55 +0800

    add

--- firestore/src/app/app.module.ts
+++ firestore/src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
+var firebaseConfig = {
+
+  };
 
 
 @NgModule({
@@ -10,7 +15,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
