1a43e2785599c2ae8c9059c05788a7002a5903b1
deazee101 <deazee101@gmail.com>
2017-11-25 03:43:56 +0800

    initial - delete

--- users-app/src/app/user-list/user-list.component.ts
+++ users-app/src/app/user-list/user-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { User } from './../user';
+
 
 @Component({
   selector: 'app-user-list',
@@ -8,6 +10,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class UserListComponent implements OnInit {
 
   @Input() users;
+  @Output() destroyUserEvent = new EventEmitter();
 
   constructor() { }
 
