5a51df5502edd827cd0184002c3881c3ed6b501e
deazee101 <deazee101@gmail.com>
2017-11-24 16:08:16 +0800

    added service

--- users-app/src/app/app.module.ts
+++ users-app/src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// import { FormsModule } from '@angular/forms';
+// import { HttpModule } from '@angular/http';
 
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserNewComponent } from './user-new/user-new.component';
+import { UserEditComponent } from './user-edit/user-edit.component';
+import { UserService } from './user.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    UserDetailsComponent,
+    UserListComponent,
+    UserNewComponent,
+    UserEditComponent,
+//    FormsModule,
+//    HttpModule
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ users-app/src/app/user.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { User } from './user';
+import "rxjs";
+import { Observable } from "rxjs";
+import { Http } from '@angular/http';
+
+@Injectable()
+export class UserService {
+
+  constructor(private _http: Http) { }
+
+  create(user: User){
+    return this._http.post("/users", user).toPromise()
+  } 
+
+  destroy(user: User){
+    return this._http.delete("/users/"+user._id).toPromise() 
+  }
+
+  update(user: User){
+    return this._http.put("/users/"+user._id, user).toPromise() 
+  }
+
+  getUsers(){
+    return this._http.get("/users/").toPromise()
+  }
+
+  getUser(user: User){
+    return this._http.get("/users/"+user._id).toPromise
+  }
+
+}
